c5eca1c9518d0ab7e0d7ed510ed73fa5469a7bbf
Dillon Redding <dillonredding@gmail.com>
2023-03-31 21:48:55 -0500

    Highlight active link's icon

--- src/components/LinkPanelBlock.tsx
+++ src/components/LinkPanelBlock.tsx
@@ -4,11 +4,19 @@ import { EmbeddedLink, Link } from '@siren-js/client';
 
 import { useBrowserContext } from '../browser';
 
+function areEqualUrls(href1: string, href2: string): boolean {
+  // wrap in URL to account for trailing slashes
+  const url1 = new URL(href1);
+  const url2 = new URL(href2);
+  return url1.href === url2.href;
+}
+
 export const LinkPanelBlock: Component<{ link: Link | EmbeddedLink }> = ({ link }) => {
-  const { follow } = useBrowserContext();
+  const { follow, location } = useBrowserContext();
   return (
     <a
       class="panel-block"
+      classList={{ 'is-active': areEqualUrls(link.href, location()) }}
       onClick={(e) => {
         e.preventDefault();
         follow(link);
