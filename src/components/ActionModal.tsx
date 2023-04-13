456e7c4b02d48c5967c3c542aa9e1b3c5056baea
Dillon Redding <dillonredding@gmail.com>
2023-04-12 21:19:29 -0500

    Add support for checkbox and file fields

--- src/components/ActionModal.tsx
+++ src/components/ActionModal.tsx
@@ -1,30 +1,15 @@
-import { Accessor, Component, For, Show } from 'solid-js';
+import { Accessor, Component, For } from 'solid-js';
 
 import { Action } from '@siren-js/client';
 
 import { useBrowserContext } from '../browser';
+import { FieldInput } from './FieldInput';
 
 interface ActionModalProps {
   action: Action;
   active: Accessor<boolean>;
   onClose: () => void;
 }
-
-const typeToIcon = new Map([
-  ['search', 'search'],
-  ['tel', 'phone'],
-  ['url', 'link'],
-  ['email', 'envelope'],
-  ['password', 'lock'],
-  ['date', 'calendar-day'],
-  ['month', 'calendar'],
-  ['week', 'calendar-week'],
-  ['time', 'clock'],
-  ['date-time', 'calendar-alt'],
-  ['datetime-local', 'calendar-alt'],
-  ['text', 'comment-alt'],
-  ['number', 'hashtag'],
-]);
 
 export const ActionModal: Component<ActionModalProps> = ({ action, active, onClose }) => {
   const { submit } = useBrowserContext();
@@ -41,33 +26,7 @@ export const ActionModal: Component<ActionModalProps> = ({ action, active, onClo
               submit(action);
             }}
           >
-            <For each={action.fields}>
-              {(field) => {
-                const type = field.type === 'date-time' ? 'datetime-local' : field.type;
-                const icon = typeToIcon.get(field.type) ?? 'question';
-                return (
-                  <Show when={field.type !== 'hidden'}>
-                    <div class="field">
-                      <label class="label">{field.title ?? field.name}</label>
-                      <div class="control has-icons-left">
-                        <input
-                          class="input"
-                          type={type}
-                          name={field.name}
-                          value={String(field.value ?? '')}
-                          onKeyUp={(e) => {
-                            field.value = e.currentTarget.value;
-                          }}
-                        />
-                        <span class="icon is-small is-left">
-                          <i class={`fas fa-${icon}`}></i>
-                        </span>
-                      </div>
-                    </div>
-                  </Show>
-                );
-              }}
-            </For>
+            <For each={action.fields}>{(field) => <FieldInput field={field} />}</For>
             <button class="button is-primary">Submit</button>
           </form>
         </div>
---
+++ src/components/FieldInput.tsx
@@ -0,0 +1,92 @@
+import { Component, createSignal, Match, Show, Switch } from 'solid-js';
+
+import { Field } from '@siren-js/client';
+
+const typeToIcon = new Map([
+  ['search', 'search'],
+  ['tel', 'phone'],
+  ['url', 'link'],
+  ['email', 'envelope'],
+  ['password', 'lock'],
+  ['date', 'calendar-day'],
+  ['month', 'calendar'],
+  ['week', 'calendar-week'],
+  ['time', 'clock'],
+  ['date-time', 'calendar-alt'],
+  ['datetime-local', 'calendar-alt'],
+  ['text', 'comment-alt'],
+  ['number', 'hashtag'],
+]);
+
+export const FieldInput: Component<{ field: Field }> = ({ field }) => {
+  const type = field.type === 'date-time' ? 'datetime-local' : field.type;
+  const icon = typeToIcon.get(field.type) ?? 'question';
+  const [filename, setFilename] = createSignal('');
+  return (
+    <Show when={field.type !== 'hidden'}>
+      <div class="field">
+        <Switch
+          fallback={
+            <>
+              <label class="label">{field.title ?? field.name}</label>
+              <div class="control has-icons-left">
+                <input
+                  class="input"
+                  type={type}
+                  name={field.name}
+                  value={String(field.value ?? '')}
+                  onKeyUp={(e) => {
+                    field.value = e.currentTarget.value;
+                  }}
+                />
+                <span class="icon is-small is-left">
+                  <i class={`fas fa-${icon}`}></i>
+                </span>
+              </div>
+            </>
+          }
+        >
+          <Match when={field.type === 'checkbox'}>
+            <div class="control">
+              <label class="checkbox">
+                <input
+                  type="checkbox"
+                  style={{ 'margin-right': '0.3rem' }}
+                  name={field.name}
+                  checked={!!field.checked}
+                  onChange={(e) => {
+                    field.checked = e.currentTarget.checked;
+                  }}
+                />
+                {field.title ?? field.name}
+              </label>
+            </div>
+          </Match>
+          <Match when={field.type === 'file'}>
+            <div class="file has-name is-fullwidth">
+              <label class="file-label">
+                <input
+                  class="file-input"
+                  type="file"
+                  name={field.name}
+                  onInput={(e) => {
+                    setFilename(e.currentTarget.value);
+                    field.value = e.currentTarget.files;
+                    console.dir(field);
+                  }}
+                />
+                <span class="file-cta">
+                  <span class="file-icon">
+                    <i class="fas fa-upload"></i>
+                  </span>
+                  <span class="file-label">Choose a file…</span>
+                </span>
+                <span class="file-name">{filename()}</span>
+              </label>
+            </div>
+          </Match>
+        </Switch>
+      </div>
+    </Show>
+  );
+};
